4b9f4fdb5c3ecfbe4a75e8ef428ffa6d2d9883a8
Marlon Mercado <marlonilaganmercado@gmail.com>
2022-02-26 17:31:38 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var display = document.getElementById('screen');
+var buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map(button => {
+  button.addEventListener('click', (e) => {
+    switch (e.target.innerText) {
+      case 'C':
+        display.innerText = '';
+        break;
+      case 'DEL':
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      case '=':
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerText = 'ERROR';
+        }
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
